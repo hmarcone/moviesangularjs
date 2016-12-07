8a21e12a24b1f7cf5d32d7c2ea35073123b9135c
hmarcone <hmarcone@gmail.com>
2016-12-07 12:38:43 -0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Filmoteca.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Title = "Home Page";
+
+			return View();
+		}
+	}
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,114 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Filmoteca.Models;
+
+namespace Filmoteca.Controllers
+{
+    public class MovieController : ApiController
+    {
+        private readonly MovieDb _db = new MovieDb();
+
+        // GET: api/Movie
+        public IQueryable<Movie> GetMovies()
+        {
+            return _db.Movies;
+        }
+
+        // GET: api/Movie/5
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _db.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+
+        // PUT: api/Movie/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Movie
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult PostMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.Movies.Add(movie);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = movie.Id }, movie);
+        }
+
+        // DELETE: api/Movie/5
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            Movie movie = _db.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _db.Movies.Remove(movie);
+            _db.SaveChanges();
+
+            return Ok(movie);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _db.Movies.Count(e => e.Id == id) > 0;
+        }
+    }
+}
