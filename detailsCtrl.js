be4f3d4b305321fdeaff34b4156f36dd995eb4ad
hmarcone <hmarcone@gmail.com>
2016-12-07 12:27:30 -0200

    Add files via upload

---
+++ detailsCtrl.js
@@ -0,0 +1,20 @@
+﻿(function(app) {
+	var detailsCtrl = function ($scope, $routeParams, movieService) {
+		var id = $routeParams.id;
+
+		movieService.getMovieById(id)
+			.success(function(data) {
+				$scope.movie = data;
+			})
+			.error(function(e) {
+				console.log(e.message);
+			});
+
+		$scope.edit = function () {
+			$scope.edit.movie = angular.copy($scope.movie);
+		};
+	};
+
+	app.controller("detailsCtrl", detailsCtrl);
+
+})(angular.module("movie"));
---
+++ listCtrl.js
@@ -0,0 +1,40 @@
+﻿(function (app) {
+	var listCtrl = function ($scope, movieService) {
+		movieService
+			.getMovies()
+			.success(function(data) {
+				$scope.movies = data;
+			});
+
+		var removeMovieById;
+
+		$scope.create = function () {
+			$scope.edit = {
+				movie: {
+					Title: "",
+					Duration: 0,
+					LaunchYear: new Date().getFullYear()
+				}
+			};
+		};
+
+		$scope.remove = function (movie) {
+			movieService.remove(movie)
+				.success(function () {
+					removeMovieById(movie.Id);
+				});
+		};
+
+		removeMovieById = function (id) {
+			for (var i = 0; i < $scope.movies.length; i++) {
+				if ($scope.movies[i].Id === id) {
+					$scope.movies.splice(i, 1);
+					break;
+				}
+			}
+		};
+	};
+
+	app.controller("listCtrl", listCtrl);
+
+}(angular.module("movie")));
